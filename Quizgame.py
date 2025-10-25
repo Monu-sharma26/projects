9dae1b470eb6ed3edc7fa2f2d7ac8f0985a2c446
Monu-sharma26 <monusharma9599637582@gmail.com>
2025-10-25 18:43:42 +0530

    Update Quizgame.py

--- Quizgame.py
+++ Quizgame.py
@@ -15,59 +15,57 @@ ques = [
     "Q9: Which is the largest animal on Earth?\nA) Elephant\tB) Blue Whale\tC) Giraffe\tD) Shark",
     "Q10: Which festival is known as the festival of lights?\nA) Holi\tB) Diwali\tC) Eid\tD) Christmas"
 ]
-print(ques)
+
+# Empty list to store user answers
+AL = []
 for i in range(len(ques)):
     st.write(ques[i])
-# Empty list to store user answers
-# AL = []
-# for i in range(len(ques)):
-#     print(ques[i])
-#     ans = st.text_input("Enter your choice (A/B/C/D): ")  
-#     AL.append(ans)
-#     print("<....................................>")
+    ans = st.text_input("Enter your choice (A/B/C/D): ")  
+    AL.append(ans)
+    print("<....................................>")
 
-# Score = 0
+Score = 0
 
-# if AL[0] == 'C' or AL[0] == "c":
-#     Score += 1
-# if AL[1] == 'D' or AL[1] == "d":
-#     Score += 1
-# if AL[2] == 'B' or AL[2] == "b":
-#     Score += 1
-# if AL[3] == 'C' or AL[3] == "c":
-#     Score += 1
-# if AL[4] == 'B' or AL[4] == "b":
-#     Score += 1
-# if AL[5] == 'B' or AL[5] == "b":
-#     Score += 1
-# if AL[6] == 'B' or AL[6] == "b":
-#     Score += 1
-# if AL[7] == 'B' or AL[7] == "b":
-#     Score += 1
-# if AL[8] == 'B' or AL[8] == "b":
-#     Score += 1
-# if AL[9] == 'B' or AL[9] == "b":
-#     Score += 1
+if AL[0] == 'C' or AL[0] == "c":
+    Score += 1
+if AL[1] == 'D' or AL[1] == "d":
+    Score += 1
+if AL[2] == 'B' or AL[2] == "b":
+    Score += 1
+if AL[3] == 'C' or AL[3] == "c":
+    Score += 1
+if AL[4] == 'B' or AL[4] == "b":
+    Score += 1
+if AL[5] == 'B' or AL[5] == "b":
+    Score += 1
+if AL[6] == 'B' or AL[6] == "b":
+    Score += 1
+if AL[7] == 'B' or AL[7] == "b":
+    Score += 1
+if AL[8] == 'B' or AL[8] == "b":
+    Score += 1
+if AL[9] == 'B' or AL[9] == "b":
+    Score += 1
 
-# print(f"\nYou scored {Score} out of {len(ques)}")
+print(f"\nYou scored {Score} out of {len(ques)}")
 
-# if Score == 10:
-#     st.balloons()
-#     print("Outstanding! You’re the topper of this quiz!")
-# elif Score == 9:
-#     st.balloons()
-#     print("Excellent performance! Keep it up!")
-# elif Score == 8:
-#     st.balloons()
-#     print("You have passed the Quiz with 8 points and got Third position")
-# elif Score == 7:
-#     st.balloons()
-#     print("You have passed the Quiz with 7 points and got Fourth position")
-# elif Score == 6:
-#     st.balloons()
-#     print("You passed the quiz. Good effort!")
-# else:
-#     print("Better Luck next time....")
-# print("\nYour Answers:", AL)
+if Score == 10:
+    st.balloons()
+    print("Outstanding! You’re the topper of this quiz!")
+elif Score == 9:
+    st.balloons()
+    print("Excellent performance! Keep it up!")
+elif Score == 8:
+    st.balloons()
+    print("You have passed the Quiz with 8 points and got Third position")
+elif Score == 7:
+    st.balloons()
+    print("You have passed the Quiz with 7 points and got Fourth position")
+elif Score == 6:
+    st.balloons()
+    print("You passed the quiz. Good effort!")
+else:
+    print("Better Luck next time....")
+print("\nYour Answers:", AL)
 
-# st.sidebar.markdown("Restart")
+st.sidebar.markdown("Restart")
